ecb81eff5e71c5968988226424a644d493d34e8c
Ashish-roka <157863836+Ashish-roka@users.noreply.github.com>
2024-03-05 08:07:37 +0545

    Initial commit

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import{Routes,Route } from 'react-router-dom'
+
+import Home from './container/home'
+import Contact from './container/contact'
+import Portfolio from './container/portfolio'
+import Skills from './container/skills'
+import Resume from './container/resume'
+import About from './container/about'
+import Navbar from './components/navBar'
+
+function App() {
+ return(
+    <div className="App">
+
+
+        {/* navbar */}
+
+
+        <Navbar/>
+        <Routes>
+            <Route index  path='/' element={<Home/>}/>
+            <Route  path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/portfolio' element={<Portfolio/>}/>
+            <Route path='/resume' element={<Resume/>}/>
+            <Route path='/skills' element={<Skills/>}/>
+        </Routes>
+    </div>
+ )
+}
+
+export default App
